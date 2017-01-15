7f8f409d5359fc64cf49fffaa2f697c100ebfc68
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2017-01-15 23:54:23 +0100

    Implemented first try of full alg.

--- src/cart/core.clj
+++ src/cart/core.clj
@@ -1,3 +1,4 @@
+;;;; CART Algorithm
 (ns cart.core)
 
 ;; Is it equal to one?
@@ -27,7 +28,7 @@
 
 ;; Find split that minimizes gini impurity 
 (defn find-split
-  "Fins split that minimize gini-impurity. If min-idx is -1 it means no split."
+  "Fins split that minimize gini-impurity. If idx is -1 it means no split."
   [xs]
   (loop [idx (- (count xs) 1)                           ; Start splitting on n-1
          min-gini (gini (avg xs))                       ; Gini on full set
@@ -36,9 +37,49 @@
           g (+ (gini p1) (gini p2))]                    ; Total gini resulting from split
       #_(println (str "i: " idx ", g: " (double g)))      ; debug output
       (if (= idx 1)                                     ; Stop at idx 1 
-        {:min-gini min-gini, :min-idx min-idx}          ; return min
+        {:gini min-gini, :idx min-idx}          ; return min
         (recur (dec idx)                                ; reduce idx by 1
                (min min-gini g)                         ; find minimum gini
                (if (> min-gini g) idx min-idx))))))     ; update min-idx if smaller
 
+
+;; Sample without replacement (Algorithm R)
+(defn sample
+  [x k]
+  {:pre [(<= k (count x))]}
+  (let [S (into [] x)
+        n (count x)]
+    (loop [i k
+           R (into [] (take k x))
+           j (rand-int (inc k))]
+      #_(println (str "i=" i " j=" j " S=" S " x[i]=" (get S i)))
+      (if (= i n)
+        R
+        (if (< j k)
+          (recur (inc i) (assoc R j (get S i)) (rand-int (+ i 2)))
+          (recur (inc i) R (rand-int (+ i 2))))))))
+
+
+;; Compute optimal split points for the choosen variables 
+(defn compute-splits
+  [data vars]
+  (map #(conj (find-split (map :y (sort-by % data))) [:var %]) vars))
+
+
+;; Train a decision tree recursively using CART
+(defn cart
+  "Train a decisiontree using CART on provided dataset."
+  [data opts]
+  (let [max-depth (:max-depth opts 5)
+        mtry (:mtry opts (int (Math/sqrt (count data))))
+        vars (disj (into #{} (keys (first data))) :y)
+        splits (compute-splits data (sample vars mtry))
+        optimal (apply min-key :gini splits)
+        split-dat (split-at (:idx optimal) (sort-by (:var optimal) data))]
+    (if (= (:idx optimal) -1)
+      (avg (map :y data))
+      {:var (:var optimal)
+       :split ((:var optimal) (first (second split-dat)))
+       :left (cart (first split-dat) opts)
+       :right (cart (second split-dat) opts)})))
 
--- test/cart/core_test.clj
+++ test/cart/core_test.clj
@@ -7,32 +7,41 @@
 (deftest nosplit-test-all-one
   (testing "Test find-split on perfect set w all 1"
     (is (= (find-split [1 1 1 1 1])
-           {:min-gini 0 :min-idx -1}))))
+           {:gini 0 :idx -1}))))
 
 
 ; Test find-split on perfect set w all zeros
 (deftest nosplit-test-all-zero
   (testing "Test find-split on perfect set w all 0"
     (is (= (find-split [0 0 0 0 0])
-           {:min-gini 0 :min-idx -1}))))
+           {:gini 0 :idx -1}))))
 
 
 ; Test find-split on perfectly sepparable set
 (deftest easysplit-test
   (testing "Test find-split on sepparable set"
     (is (= (find-split [1 1 1 0 0])
-           {:min-gini 0 :min-idx 3}))))
+           {:gini 0 :idx 3}))))
 
 
 ; Test find-split on perfectly sepparable set mirrored
 (deftest easysplit-test-mirror
   (testing "Test find-split on sepparable set mirrored"
     (is (= (find-split [0 0 0 1 1])
-           {:min-gini 0 :min-idx 3}))))
+           {:gini 0 :idx 3}))))
 
 
 ; Test find-split on perfectly sepparable set
 (deftest hardsplit-test
   (testing "Test find-split on non sepparable set"
     (is (= (find-split [1 0 1 0 1])
-           {:min-gini 12/25 :min-idx -1}))))
+           {:gini 12/25 :idx -1}))))
+
+
+;;;; Read data prom file and parse into vector of maps
+;;(def data
+;;  (rest (map (fn [line] 
+;;               (into {} 
+;;                     (map #(vector %1 (read-string %2))
+;;                          [:x1 :x2 :y] (split line #",")))) 
+;;             (split (slurp "resources/data.csv") #"\n"))))
